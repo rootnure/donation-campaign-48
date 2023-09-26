09590557c7975bc99a5f07084ce00773644ae87a
rootnure <nur.diu.2791@gmail.com>
2023-09-26 14:49:36 +0600

    Donation Details loading functionality added for UI in Dynamic Route

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -4,16 +4,29 @@ import { useLoaderData, useParams } from "react-router-dom";
 const DonationDetails = () => {
 
     const donations = useLoaderData();
-    const {donationId} = useParams();
+    const { donationId } = useParams();
 
     const donation = donations.find(donation => donation.id === parseInt(donationId));
 
-    console.log(donations, donationId, donation);
+    const { title, image, color, description, donation_amount } = donation;
 
     return (
-        <div>
-            donation details
-        </div>
+        <main className='container mx-auto min-h-[calc(100vh-250px)]'>
+            <section className="space-y-6">
+                <div className="card my-12">
+                    <figure className="max-h-[500px] rounded-md"><img src={image.main} alt={`Banner image of ${title}`} /></figure>
+                    <div className="relative z-20">
+                        <div className="card-actions bottom-0 absolute bg-[#00000099] w-full rounded-md rounded-t-none">
+                            <button className={`btn border-0 hover:text-${color.main} my-8 ms-6 text-white font-bold normal-case bg-${color.main}`}>Donate ${donation_amount}</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="space-y-6">
+                    <h2 className="text-3xl font-bold">{title}</h2>
+                    <p className="text-justify">{description}</p>
+                </div>
+            </section>
+        </main>
     );
 };
 
