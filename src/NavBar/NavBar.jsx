64cfaea969c5b770c1c6e7217bda10de8c092612
rootnure <nur.diu.2791@gmail.com>
2023-09-26 12:21:36 +0600

    Navbar added with active route styles

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,9 +1,21 @@
+import { Link, NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
-        <nav>
-            <h2 className="text-3xl">Navbar</h2>
+        <nav className="py-4">
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <Link to="/">
+                        <img src="https://i.ibb.co/2y1bQYt/logo.png" alt="Website Logo" />
+                    </Link>
+                </div>
+                <div className="flex-none space-x-8 text-xl">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/donation">Donation</NavLink>
+                    <NavLink to="/statistics">Statistics</NavLink>
+                </div>
+            </div>
         </nav>
     );
 };
